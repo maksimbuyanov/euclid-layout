522dff8b5e2379601b69f2cf972d943407141d08
maksimbuyanov <maksi_777@bk.ru>
2022-06-29 16:30:57 +0700

    slider + accordion

--- js/main.js
+++ js/main.js
@@ -1,26 +1,70 @@
 const burgerButton = document.querySelector('.header__menu-button')
 const nav = document.querySelector('.nav')
 
-burgerButton.addEventListener('click', function() {
+burgerButton.addEventListener('click', function () {
   this.classList.toggle('header__menu-button_closed')
   nav.classList.toggle('nav_visible')
   document.body.classList.toggle('block-scroll')
 })
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
-
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true
   },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper__button-next',
-    prevEl: '.swiper__button-prev',
+  effect: "fade",
+  autoplay: {
+    delay: 3000,
   },
+});
+
+const howStepArr = document.querySelectorAll('.how__step')
+const howItem = document.querySelector('.how__item')
+const howSubtitleArr = document.querySelectorAll('.how__subtitle')
+const howTextArr = document.querySelectorAll('.how__text')
+
+const clearActive = () => {
+  howStepArr.forEach(step => {
+    step.classList.remove('how__step_active')
+  })
+  howSubtitleArr.forEach(subtitle => {
+    subtitle.classList.add('hidden')
+  })
+  howTextArr.forEach(text => {
+    text.classList.add('hidden')
+  })
+  howItem.classList.forEach(className => {
+    if (className !== 'how__item') {
+      howItem.classList.remove(className)
+    }
+  })
+}
+
+howStepArr.forEach(step => {
+  step.addEventListener('click',()=> {
+    clearActive()
+    const targetData = step.dataset.step
+    howStepArr.forEach(step => {
+      if(step.dataset.step === targetData) {
+        step.classList.add('how__step_active')
+      }
+    })
+    howItem.classList.add(`how__item_${targetData}`)
+    howSubtitleArr.forEach(subtitle => {
+      if(subtitle.dataset.step === targetData) {
+        subtitle.classList.remove('hidden')
+      }
+    })
+    howTextArr.forEach(text => {
+      if(text.dataset.step === targetData) {
+        text.classList.remove('hidden')
+      }
+    })
+  })
+})
+
+$(".faq__list").accordion({
+  heightStyle: "content",
 });
